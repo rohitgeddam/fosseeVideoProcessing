2bccf08193bcaa7a39966d9361f7c8a18fb9ec17
rohitgeddam <rohitgeddam2018@gmail.com>
2021-01-01 14:13:21 +0530

    display result table

--- ui/src/sections/Editing/ResultsTable.tsx
+++ ui/src/sections/Editing/ResultsTable.tsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { Table } from 'antd';
+
+const columns = [
+    {
+        title: 'Sr. No',
+        dataIndex: 'me',
+        key: 'me',
+      },
+      {
+        title: 'Start Time',
+        dataIndex: 'startTime',
+        key: 'startTime',
+      },
+      {
+        title: 'End Time',
+        dataIndex: 'endTime',
+        key: 'endTime',
+      },
+      {
+        title: 'Subtitle',
+        dataIndex: 'subtitleChunk',
+        key: 'subtitleChunk',
+      },
+      {
+        title: 'Video',
+        dataIndex: 'videoChunkPath',
+        key: 'videoChunkPath',
+        render: (link: string) => <a href={`http://localhost:8000${link}`} target="_blank">view</a>
+      },
+      {
+        title: 'Audio',
+        dataIndex: 'audioChunkPath',
+        key: 'audioChunkPath',
+        render: (link: string) => <a href={`http://localhost:8000${link}`} target="_blank">view</a>
+
+      },
+]
 
 export const ResultsTable =  ({result}: any) => {
+
+    const [dataSource, setDataSource] = useState<any>(null)
+    // const [column, setColumn] = useState(null);
+
+    useEffect(() => {
+        const data = result.chunks.map((chunk: any) => {
+            return {
+                // key: chunk.me,
+                // id: chunk.id,
+                me: chunk.me,
+                // operationId: chunk.operationId,
+                startTime: chunk.startTime,
+                endTime: chunk.endTime,
+                subtitleChunk: chunk.subtitleChunk,
+                // videoChunkName: chunk.videoChunkName,
+                videoChunkPath: chunk.videoChunkPath,
+                // audioChunkName: chunk.audioChunkName,
+                audioChunkPath: chunk.audioChunkPath,
+            }
+            
+        })
+        setDataSource(data);
+
+    }, [])
     return (
-        <h1>{result.message}</h1>
+        <Table dataSource={dataSource} columns={columns}/>
     )
 }
 
